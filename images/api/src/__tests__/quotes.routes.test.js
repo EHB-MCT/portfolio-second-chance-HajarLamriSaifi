44758937324f85acb45d35dbdfe17a85d708073e
hajar <hajar.lamri.saifi@student.ehb.be>
2023-08-19 20:42:44 +0200

    feat: Add quotes route testing

--- images/api/src/__tests__/quotes.routes.test.js
+++ images/api/src/__tests__/quotes.routes.test.js
@@ -3,69 +3,123 @@ const app = require("../index");
 
 const request = supertest(app);
 
-describe("POST /user", () => {
+/**
+ * Test suite for Quote routes
+ * @module tests/quoteRoutes
+ */
+
+let userId;  // To store the dynamically created user's ID
+
+/**
+ * Test suite for creating a quote
+ */
+describe("POST /quotes", () => {
+  /**
+   * Create a user before running the quote tests.
+   */
+  beforeAll(async () => {
+    const userResponse = await request.post("/user").send({
+      username: "testUser",
+      password: "testPassword",
+    });
+    userId = userResponse.body.id;
+  });
+
+  /**
+   * Test that verifies a quote can be successfully created.
+   */
   test("should respond with code 200", async () => {
-    const response = await request.post("/user").send({
-      username: "username",
-      password: "password",
+    const response = await request.post("/quotes").send({
+      quote: "A sample quote",
+      user_id: userId  // Use the dynamically created user's ID
     });
     expect(response.statusCode).toBe(200);
   });
-});
 
-describe("GET /user", () => {
-  test("should respond with code 200", async () => {
-    const response = await request.get("/user");
-    expect(response.statusCode).toBe(200);
-  });
-
-  test("should return an array", async () => {
-    const response = await request.get("/user");
-    expect(Array.isArray(response.body)).toBe(true);
-  });
-});
-
-describe("PUT /user/:id", () => {
-  test("should respond with code 200 when updating an existing user", async () => {
-    const createUserResponse = await request.post("/user").send({
-      id: "1",
-      username: "username",
-      password: "password",
+  /**
+   * Test suite for fetching quotes
+   */
+  describe("GET /quotes", () => {
+    /**
+     * Test that verifies the route responds with a 200 status code.
+     */
+    test("should respond with code 200", async () => {
+      const response = await request.get("/quotes");
+      expect(response.statusCode).toBe(200);
     });
 
-    const response = await request.put("/user/1").send({
-      id: "1",
-      username: "username",
-      password: "updatedPassword",
+    /**
+     * Test that verifies the response contains an array (list of quotes).
+     */
+    test("should return an array", async () => {
+      const response = await request.get("/quotes");
+      expect(Array.isArray(response.body)).toBe(true);
     });
-
-    expect(response.statusCode).toBe(200);
   });
 
-  test("should respond with code 404 when trying to update a non-existent user", async () => {
-    const response = await request.put("/user/999").send({
-      username: "username",
-      password: "updatedPassword",
+  /**
+   * Test suite for updating a quote
+   */
+  describe("PUT /quotes/:id", () => {
+    /**
+     * Test that verifies an existing quote can be successfully updated.
+     */
+    test("should respond with code 200 when updating an existing quote", async () => {
+      const createQuoteResponse = await request.post("/quotes").send({
+        quote: "Initial quote",
+        user_id: userId
+      });
+
+      const response = await request.put(`/quotes/${createQuoteResponse.body.id}`).send({
+        quote: "Updated quote"
+      });
+
+      expect(response.statusCode).toBe(200);
     });
 
-    expect(response.statusCode).toBe(404);
-  });
-});
+    /**
+     * Test that verifies the behavior when trying to update a quote that doesn't exist.
+     */
+    test("should respond with code 404 when trying to update a non-existent quote", async () => {
+      const response = await request.put("/quotes/999").send({
+        quote: "Some quote"
+      });
 
-describe("DELETE /user/:id", () => {
-  test("should respond with code 200 when deleting an existing user", async () => {
-    const createUserResponse = await request.post("/user").send({
-      id: "2",
-      username: "username",
-      password: "password",
+      expect(response.statusCode).toBe(404);
+    });
+  });
+
+  /**
+   * Test suite for deleting a quote
+   */
+  describe("DELETE /quotes/:id", () => {
+    /**
+     * Test that verifies an existing quote can be successfully deleted.
+     */
+    test("should respond with code 200 when deleting an existing quote", async () => {
+      const createQuoteResponse = await request.post("/quotes").send({
+        quote: "Another sample quote",
+        user_id: userId
+      });
+
+      const response = await request.delete(`/quotes/${createQuoteResponse.body.id}`);
+      expect(response.statusCode).toBe(200);
     });
 
-    const response = await request.delete("/user/2");
-    expect(response.statusCode).toBe(200);
+    /**
+     * Test that verifies the behavior when trying to delete a quote that doesn't exist.
+     */
+    test("should respond with code 404 when trying to delete a non-existent quote", async () => {
+      const response = await request.delete("/quotes/999");
+      expect(response.statusCode).toBe(404);
+    });
   });
 
-  test("should respond with code 404 when trying to delete a non-existent user", async () => {
-    const response = await request.delete("/user/999");
-    expect(response.statusCode).toBe(404);
+  /**
+   * Cleanup after all tests are done.
+   */
+  afterAll(async () => {
+    // Optionally, delete the test user to clean up the database
+    await request.delete(`/user/${userId}`);
   });
 });
