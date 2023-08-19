e1654239b45116a3e0ac3bf5526af97578de50b8
hajar <hajar.lamri.saifi@student.ehb.be>
2023-08-19 19:39:50 +0200

    feat: Create users route with CRUD operations

---
+++ images/api/src/routes/users.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const knex = require("../db/db");
+
+router.post("/", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const newUser = await knex("users").insert({ username, password });
+    console.log(`User with the name "${username}" created.`);
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(`Error creating user "${username}":`, error.message);
+    res.status(500).json({ message: "Error creating user", error });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await knex("users").select();
+    console.log("Retrieved all users.");
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error retrieving users:", error.message);
+    res.status(500).json({ message: "Error retrieving users", error });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await knex("users").where("id", req.params.id).first();
+    if (user) {
+      console.log(`Retrieved user with ID: ${req.params.id}`);
+      res.status(200).json(user);
+    } else {
+      console.log(`User with ID: ${req.params.id} not found.`);
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error(
+      `Error retrieving user with ID: ${req.params.id}`,
+      error.message
+    );
+    res.status(500).json({ message: "Error retrieving user", error });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    await knex("users")
+      .where("id", req.params.id)
+      .update({ username, password });
+    console.log(`User with ID: ${req.params.id} updated.`);
+    res.status(200).json({ message: "User updated" });
+  } catch (error) {
+    console.error(
+      `Error updating user with ID: ${req.params.id}`,
+      error.message
+    );
+    res.status(500).json({ message: "Error updating user", error });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await knex("users").where("id", req.params.id).del();
+    console.log(`User with ID: ${req.params.id} deleted.`);
+    res.status(200).json({ message: "User deleted" });
+  } catch (error) {
+    console.error(
+      `Error deleting user with ID: ${req.params.id}`,
+      error.message
+    );
+    res.status(500).json({ message: "Error deleting user", error });
+  }
+});
+
+module.exports = router;
