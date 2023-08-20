9ac8d86a643650dc2d1920adcbd12e6d6afb0e78
hajar <hajar.lamri.saifi@student.ehb.be>
2023-08-20 12:43:23 +0200

    feat: Created login components

---
+++ images/frontend/src/components/main/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../login/Login";
+import "../main/main.scss";
+
+function Main() {
+  return (
+    <div>
+      Main
+      <Login />
+    </div>
+  );
+}
+
+export default Main;
